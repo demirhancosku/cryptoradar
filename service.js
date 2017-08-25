a17fcd82ac906ee8155b66c4f7488c8aebc65807
Kagan Ozupek <kaganozupek@Kagans-MacBook-Pro-2.local>
2017-08-25 16:43:43 +0300

    Done

--- service.js
+++ service.js
@@ -18,9 +18,7 @@ const buyService = new BuyService();
 const sellService = new SellService();
 
 
-var query = require('cli-interact').getYesNo;
-var answer = query('Is it true');
-console.log('you answered:', answer);
+
 
 async function init() {
 
